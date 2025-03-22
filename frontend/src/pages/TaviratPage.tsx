906e0f17aedd62d027c0b8845fca94c27d1f8b11
Csaba44 <csabi16sheldon@gmail.com>
2025-03-22 21:52:48 +0100

    FlightplanToText utility function added

--- frontend/src/pages/TaviratPage.tsx
+++ frontend/src/pages/TaviratPage.tsx
@@ -12,13 +12,8 @@ function TaviratPage() {
         </div>
 
         <div className="flex pt-1">
-          <div className="w-[90vw] bg-[#868686] h-[50vh] overflow-y-scroll overflow-x-hidden text-cyan-200 pl-1">KTF 799 121642 <br />FF LHKRZEZX <br />121642 EUCHZMFP(FPL-BCS7837-IS
-<br />-A306/H-SDFGHIJ3RVWXYZ/LB1
-<br />-LGAV1835
-<br />-N0418F240 KRO G33 NEVRA Z507 URUDI/N0443F280 UZ507 KERES/N0449F300 UZ507 TSL UL617 MAKED DCT LONTA UM749 UDVAR DCT NANER/N0444F320 DCT BABIT DCT BEGLA DCT NAVTI DCT BODAL DCT LEGAZ DCT BEFRE/N0438F300 DCT IVQOQ/N0359F160 T951 YAWOY YAWOY1R
-<br />-EDDP0251 EKCH EDDK
-<br />-PBN/A1B1D1L1O1 DAT/CPDLCX SUR/260B DOF/250212 REG/EILDA EET/LWSS0043 LYBA0055 LHCC0131 LOVV0151 LKAA0200 EDUU0220 EDMM0229 SEL/AFJQ RVR/075 IFP/MODESASP OPR/ABR ORGN/RSYFP5O RMK/TCAS EQUIPPED 24H OPS XXXXXXXXXX )</div>
-          
+          <div className="w-[90vw] bg-[#868686] h-[50vh] overflow-y-scroll overflow-x-hidden text-cyan-200 pl-1"></div>
+
           <div className="bg-[#868686] p-1 ml-5 flex flex-col w-13 text-center">
             <span className="text-lime-700 font-bold">KTF</span>
             <span className="text-lime-700 font-bold">TKF</span>
