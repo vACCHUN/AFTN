54cce26e5d46a21fc062e897a68f7b2c12e92135
Csörgő Csaba <csabi16sheldon@gmail.com>
2025-04-12 20:36:44 +0200

    footer for távirat

--- frontend/src/pages/TaviratPage.tsx
+++ frontend/src/pages/TaviratPage.tsx
@@ -14,9 +14,49 @@ function TaviratPage() {
         <div className="flex pt-1">
           <div className="w-[90vw] bg-[#868686] h-[50vh] overflow-y-scroll overflow-x-hidden text-cyan-200 pl-1"></div>
 
+
           <div className="bg-[#868686] p-1 ml-5 flex flex-col w-13 text-center">
             <span className="text-lime-700 font-bold">KTF</span>
             <span className="text-lime-700 font-bold">TKF</span>
+          </div>
+        </div>
+
+        <div className="flex gap-4 p-2">
+          <div className="flex flex-col">
+            <div className="flex gap-2">
+              <input type="checkbox" />
+              Automatikus frissítés
+            </div>
+
+            <div className="flex gap-2">
+              <WindowsButton className="px-4">A</WindowsButton>
+              <WindowsButton className="px-4">A</WindowsButton>
+              <WindowsButton className="px-4">A</WindowsButton>
+              <WindowsButton className="px-4">A</WindowsButton>
+              <WindowsButton className="px-4">A</WindowsButton>
+            </div>
+          </div>
+
+          <div className="flex gap-4 w-full justify-end pr-10">
+            <div className="flex flex-col">
+              <strong>A szűréskor keresett szavak</strong>
+              <div className="flex gap-2">
+                <input className="border" type="text" />
+                <input className="border" type="text" />
+                <input className="border" type="text" />
+              </div>
+            </div>
+
+            <div className="flex flex-col">
+              <div className="flex gap-1">
+                <input type="radio" id="filterOn" name="filtering" />
+                <label htmlFor="filterOn" className="font-bold">Szűrés BE</label>
+              </div>
+              <div className="flex gap-1">
+                <input type="radio" id="filterOff" name="filtering" />
+                <label htmlFor="filterOff" className="font-bold">Szűrés KI</label>
+              </div>
+            </div>
           </div>
         </div>
       </div>
