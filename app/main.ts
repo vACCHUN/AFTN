b0005ee3274ecf220dedae124cc898dc9cf1c0db
Csaba44 <csabi16sheldon@gmail.com>
2026-01-05 20:00:51 +0100

    Feature: 4:3 resizing

--- app/main.ts
+++ app/main.ts
@@ -2,19 +2,23 @@ import { app, BrowserWindow, ipcMain, Menu, screen, nativeTheme } from "electron
 import "dotenv/config";
 let mainWindow: BrowserWindow | null;
 
+const WINDOW_SIZE = 0.7;
+
 app.whenReady().then(async () => {
   const { width, height } = screen.getPrimaryDisplay().workAreaSize;
 
   mainWindow = new BrowserWindow({
-    width: Math.floor(width * 0.55),
-    height: Math.floor(height * 0.95),
+    width: Math.floor(width * WINDOW_SIZE),
+    height: Math.floor((width * WINDOW_SIZE * 3) / 4),
+    resizable: true,
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true,
       preload: __dirname + "/preload.js",
     },
-    resizable: false,
   });
+
+  mainWindow.setAspectRatio(4 / 3);
 
   await mainWindow.webContents.session.clearCache();
 
